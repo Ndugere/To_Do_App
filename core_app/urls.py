acb5267bcdac6c05fee39b3b546eed02334561f9
Ndugere <ndungudavid972@gmail.com>
2025-08-01 14:18:39 +0300

    This is the second commit that gives a full todo app that accept input and display it

--- core_app/urls.py
+++ core_app/urls.py
@@ -5,5 +5,5 @@ app_name = "To_do_app"
 
 urlpatterns = [
     path("", views.tasks_view, name="tasks_view"),
-    path("add", views.add_a_task, name="add"),
+    path("add/", views.add_a_task, name="add"),
 ]
--- core_app/views.py
+++ core_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 all_tasks = ["far", "foo", "bars"]
@@ -11,4 +12,9 @@ def tasks_view(request):
     })
 
 def add_a_task(request):
+    if request.method == "POST":
+        task = request.POST.get("task")
+        if task:
+            all_tasks.append(task)
+            return HttpResponseRedirect(reverse("To_do_app:tasks_view"))
     return render(request, "core_app/add.html")
